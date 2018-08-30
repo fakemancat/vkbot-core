34c894595bcbe6fa7e4cc1d9e8aea68c709c2d2e
Fakeman Cat <33839807+fakemancat@users.noreply.github.com>
2018-08-31 00:39:27 +0500

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -1,6 +1,6 @@
 module.exports = {
   regexp: /^тест$/,
-  func: function(msg, botN) {
+  func: function(msg, { botN }) {
     msg.ok(`Бот успешно работает на ядре Fakeman Cat! Введите ${botN}, помощь для списка команд.`);
   },
   help: 'test',
