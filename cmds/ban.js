269ea241b6403520ae3d5f6976c73a6f27b58d49
Fakeman Cat <33839807+fakemancat@users.noreply.github.com>
2018-08-31 22:35:51 +0500

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,12 +1,12 @@
 module.exports = {
     tag: ['ban', 'бан', 'забанить', 'забань'],
-    func: async(msg, { vk, db }) => {
+    func: async(msg, { db }) => {
         let ID = msg.fwds[0] ? msg.fwds[0].from_id : msg.text.split(' ')[1];
         let reason = (msg.fwds[0] ? msg.text.split(' ').slice(1).join(' ') : msg.text.split(' ').slice(2).join(' ')).replace(/\n/g, " ");
         if (!ID || ID < 0 || isNaN(ID)) return msg.error('Укажите правильный айди'); // Если айди не указан или айди меньше нуля (группа) или айди не равен числу, то пишем, что нужно указать айди.
         if (!reason || reason.length > 20) return msg.error('Укажите причину бана длиной не больше 20 символов');
 
-        let user = await db.getUser(ID, { db, vk });
+        let user = await db.getUser(ID);
         if (user.ban.isBanned) return msg.error('Этот пользователь уже забанен');
 
         user.ban.isBanned = true;
