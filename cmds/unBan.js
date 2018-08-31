327083fe1c5981f1ae27103724af12576015efff
Fakeman Cat <33839807+fakemancat@users.noreply.github.com>
2018-08-31 22:35:10 +0500

    Update unBan.js

--- cmds/unBan.js
+++ cmds/unBan.js
@@ -1,10 +1,10 @@
 module.exports = {
     tag: ['unban', 'разбан', 'разабанить', 'разабань'],
-    func: async(msg, { vk, db }) => {
+    func: async(msg, { db }) => {
         let ID = msg.fwds[0] ? msg.fwds[0].from_id : msg.text.split(' ')[1];
 
         if (!ID || ID < 0 || isNaN(ID)) return msg.error('Укажите правильный айди'); // Если айди не указан или айди меньше нуля (группа) или айди не равен числу, то пишем, что нужно указать айди.
-        let user = await db.getUser(ID, { db, vk });
+        let user = await db.getUser(ID);
         if (!user.ban.isBanned) return msg.error('Этот пользователь уже разабанен');
 
         user.ban.isBanned = false;
