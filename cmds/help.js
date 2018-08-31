46af540edfc1b8ba8b2eec178dd0527959b4e158
Fakeman Cat <33839807+fakemancat@users.noreply.github.com>
2018-08-31 21:33:36 +0500

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,12 +4,31 @@ module.exports = {
     let result = [
       `&#128215; | Команды бота ${botN}.`,
       ``,
-      cmds.map((cmd) => `&#128313; | ${botN}, ${cmd.help} -- ${cmd.desc}`).join('\n'),
+      `&#128313; Команды для Пользователей &#128313;`,
+      cmds
+      .filter(cmd => cmd.rights == 0)
+      .map(cmd => `[&#128312;] ${botN}, ${cmd.help} -- ${cmd.desc}`).join('\n') || '[&#128312;] Нет команд для Пользователей',
+      ``,
+      `&#128313; Команды для Випов &#128313;`,
+      cmds
+      .filter(cmd => cmd.rights == 1)
+      .map(cmd => `[&#128312;] ${botN}, ${cmd.help} -- ${cmd.desc}`).join('\n') || '[&#128312;] Нет команд для Випов',
+      ``,
+      `&#128313; Команды для Админов &#128313;`,
+      cmds
+      .filter(cmd => cmd.rights == 2)
+      .map(cmd => `[&#128312;] ${botN}, ${cmd.help} -- ${cmd.desc}`).join('\n') || '[&#128312;] Нет команд для Админов',
+      ``,
+      `&#128313; Команды для создателя &#128313;`,
+      cmds
+      .filter(cmd => cmd.rights == 3)
+      .map(cmd => `[&#128312;] ${botN}, ${cmd.help} -- ${cmd.desc}`).join('\n') || '[&#128312;] Нет команд для Создателя',
       ``,
       `&#128203; | Бот основан на ядре от [id236908027|Fakeman Cat]`
     ].join('\n');
     msg.send(result);
   },
+  rights: 0,
   help: 'помощь',
   desc: 'список команд'
 };
