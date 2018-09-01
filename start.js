51d7f3077a91b071868ef5ba004e266b9ccd85f4
Fakeman Cat <33839807+fakemancat@users.noreply.github.com>
2018-09-01 21:44:57 +0500

    Update start.js

--- start.js
+++ start.js
@@ -31,12 +31,13 @@ db.getUser = async(ID) => {
   return user;
 };
 
+// Объявляем объект с командами
 const cmds = fs
   .readdirSync(`${__dirname}/cmds/`)
   .filter((name) => /\.js$/i.test(name))
   .map((name) => require(`${__dirname}/cmds/${name}`));
 
-// Определение профиля/группы, токена
+// Определяем профиль/группу, токен
 if (config.group_id > 0 || config.group_id != 0) {
   vk.setOptions({
     'token': config.token,
@@ -47,7 +48,7 @@ else {
   vk.setToken(config.token);
 }
 
-// Определение имени бота
+// Определяем имя бота
 let botN = '';
 if (!config.bot_name_string || config.bot_name_string == '') {
   return console.log('Укажите имя бота в файле config.js'.red.bold);
@@ -56,46 +57,72 @@ else {
   botN = config.bot_name_string;
 }
 
+// Консолим умпешный запуск
 console.log(`Бот на ядре Fakeman Cat успешно запущен. Введите команду боту в ВК: ${botN}, тест`.green.bold);
 
-// Начало обработчика
+// Запускаем Полинг (Polling)
 vk.updates.startPolling();
+
+// Запускаем обработчик новый и изменённых сообщений
 vk.updates.on(['new_message', 'edit_message'], async(msg) => {
-  if (msg.senderId < 1 || msg.isOutbox) return;
+  // Если сообщение от группы или исходящее, то возвращаем
+  if (msg.senderId < 1 || msg.isOutbox) {
+    return;
+  }
+  // Если в сообщении нет обращения к боту и если это чат, то возвращаем
+  if (!config.bot_name.test(msg.text) && msg.isChat) {
+    return;
+  }
 
-  if (!config.bot_name.test(msg.text) && msg.isChat) return;
+  // Консолим сообщения
   console.log(msg.subTypes[0] + ` ${msg.senderId} => ${msg.text}`.green.bold);
-  msg.setActivity();
-  await msg.loadMessagePayload();
-  msg.text = msg.text.replace(config.bot_name, '');
-  msg.user = await db.getUser(msg.senderId);
-  msg.fwds = msg.forwards || null;
 
+  // Делаем так, чтобы бот писал...
+  msg.setActivity();
+
+  // Загружаем весь paylaod. Нужно от возникновения ошибок
+  await msg.loadMessagePayload();
+
+  // Объявление важных переменных:
+  msg.text = msg.text.replace(config.bot_name, ''); // Текст сообщения равен тексте без имени бота: Кот, привет => привет
+  msg.user = await db.getUser(msg.senderId); // Переменная содержащая в себе информацию о пользователе из базы
+  msg.fwds = msg.forwards || null; // Просто упрощение..
+
+  // Определяем команду по regexp или tag. Если команды нет, то пишем об этом  
+  let cmd = cmds.find(cmd => cmd.regexp ? cmd.regexp.test(msg.text) : (new RegExp(`^\\s*(${cmd.tag.join('|')})`, "i")).test(msg.text));
+  if (!cmd) return msg.send('&#128213; | Команда не найдена');
+
+  // Функции "отправлялки" сообщений
+  msg.answer = (text = "", params = {}) => {
+    const result = msg.isChat ? `${config.rightIcons[msg.user.rights]} ${msg.user.nick},\n${text}` : `${text}`;
+    return msg.send(result, params);
+  };
+  msg.ok = (text = "", params = {}) => {
+    return msg.answer('&#128215; | ' + text, params);
+  };
+  msg.error = (text = "", params = {}) => {
+    return msg.answer('&#128213; | ' + text, params);
+  };
+
+  // Проверки пользователя
+  if (msg.user.rights < cmd.rights) {
+    return msg.error(`Команда доступна только ${[,'Випам', 'Админам', 'Создателю'][cmd.rights]} ${cmd.rights > 0 && cmd.rights !== 3 ? 'или выше' : ''}`);
+  }
   if (msg.user.ban.isBanned) {
     return msg.send(`&#128213; | Вы забанены по причине: "${msg.user.ban.reason}"`);
   }
 
-  let cmd = cmds.find(cmd => cmd.regexp ? cmd.regexp.test(msg.text) : (new RegExp(`^\\s*(${cmd.tag.join('|')})`, "i")).test(msg.text));
-
-  if (!cmd) return msg.send('&#128213; | Команда не найдена');
-  msg.ok = (text = "", params = {}) => {
-    return msg.send('&#128215; | ' + text, params);
-  };
-  msg.error = (text = "", params = {}) => {
-    return msg.send('&#128213; | ' + text, params);
-  };
-  if (msg.user.rights < cmd.rights) {
-    return msg.error(`Команда доступна только ${[,'Випам', 'Админам', 'Создателю'][cmd.rights]} ${cmd.rights > 0 && cmd.rights !== 3 ? 'или выше' : ''}`);
-  }
+  // Выполнение функции через try { ... } catch() { ... }
   try {
     await cmd.func(msg, { botN, cmds, vk, VK, cmd, db });
   }
   catch (e) {
     console.log(`Ошибка:\n${e}`.red.bold);
-    msg.send(`Ошибка при выполнении команды '${msg.text}'`);
+    msg.error(`Ошибка при выполнении команды '${msg.text}'`);
   }
 });
 
+// Консолим ошибки
 process.on("uncaughtException", e => {
   console.log(e);
 });
